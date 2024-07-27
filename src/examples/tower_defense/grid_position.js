72c63fdc77efe0e5fb31e70f461438fc05d5056d
Kuba Karpierz <mr.karpierz@gmail.com>
2024-07-27 13:37:36 -0400

    moved grid pos out

---
+++ src/examples/tower_defense/grid_position.js
@@ -0,0 +1,38 @@
+import { Vector3, Vector2 } from "three";
+
+export class GridPosition extends Vector2 {
+  static gridSize = 0.5;
+
+  constructor(x, y) {
+    if (x.isVector3) {
+      super(
+        Math.round(x.x / GridPosition.gridSize),
+        Math.round(x.z / GridPosition.gridSize)
+      );
+    } else {
+      super(Math.round(x), Math.round(y));
+    }
+  }
+
+  toVector3() {
+    return new Vector3(
+      this.x * GridPosition.gridSize,
+      0.3,
+      this.y * GridPosition.gridSize
+    );
+  }
+
+  neighbors() {
+    const { x, y } = this;
+    return [
+      new GridPosition(x + 1, y),
+      new GridPosition(x - 1, y),
+      new GridPosition(x, y + 1),
+      new GridPosition(x, y - 1),
+    ];
+  }
+
+  key() {
+    return this.x + "-" + this.y;
+  }
+}
--- src/examples/tower_defense/tower_defense.js
+++ src/examples/tower_defense/tower_defense.js
@@ -4,44 +4,7 @@ import { Entity } from "../../engine/entity.js";
 import { GameState } from "../../engine/engine.js";
 import { GameOverMenu } from "./game_over_menu.js";
 import { KeyedMap, KeyedSet } from "../../utils/helper.js";
-import { Vector2 } from "three";
-
-class GridPosition extends Vector2 {
-  static gridSize = 0.5;
-
-  constructor(x, y) {
-    if (x.isVector3) {
-      super(
-        Math.round(x.x / GridPosition.gridSize),
-        Math.round(x.z / GridPosition.gridSize)
-      );
-    } else {
-      super(Math.round(x), Math.round(y));
-    }
-  }
-
-  toVector3() {
-    return new Vector3(
-      this.x * GridPosition.gridSize,
-      0.3,
-      this.y * GridPosition.gridSize
-    );
-  }
-
-  neighbors() {
-    const { x, y } = this;
-    return [
-      new GridPosition(x + 1, y),
-      new GridPosition(x - 1, y),
-      new GridPosition(x, y + 1),
-      new GridPosition(x, y - 1),
-    ];
-  }
-
-  key() {
-    return this.x + "-" + this.y;
-  }
-}
+import { GridPosition } from "./grid_position.js";
 
 class Enemy extends Entity {
   constructor(position) {
