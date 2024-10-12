2d3d29f46cf39b1eb3635e08e07e4aec62c6756f
Kuba Karpierz <mr.karpierz@gmail.com>
2024-10-12 14:57:54 -0400

    config

---
+++ src/engine/data.js
@@ -0,0 +1,96 @@
+import GUI from "lil-gui";
+
+const stateString = "state";
+const configString = "config";
+
+const enumConfig = (displayName, initialValue, options) => {
+  return {
+    name: displayName,
+    value: initialValue,
+    minOrOptions: options,
+  };
+};
+
+const numberConfig = (displayName, initialValue, min, max, step) => {
+  return {
+    name: displayName,
+    value: initialValue,
+    minOrOptions: min,
+    max,
+    step,
+  };
+};
+
+class DataManager {
+  constructor({ state = {}, config = {} }) {
+    this.defaultConfig = config;
+    this.defaultState = state;
+  }
+
+  init() {
+    this.gui = new GUI();
+    // load in the local data, if any
+    this.readData();
+  }
+
+  addButton({ fn, name }) {
+    const s = {};
+    s[name] = fn;
+    this.gui.add(s, name);
+  }
+
+  addConfigData(key) {
+    const {
+      minOrOptions = null,
+      max = null,
+      step = null,
+      name,
+    } = this.config[key];
+    this.gui
+      .add(this.config[key], "value", minOrOptions, max, step)
+      .name(name)
+      .onChange(() => this.writeData());
+  }
+
+  readData() {
+    const state = localStorage.getItem(stateString);
+    if (state) {
+      this.state = JSON.parse(state);
+    } else {
+      this.state = this.defaultState;
+    }
+
+    const config = localStorage.getItem(configString);
+    if (config) {
+      this.config = JSON.parse(config);
+    } else {
+      this.config = this.defaultConfig;
+    }
+
+    for (var key of Object.keys(this.defaultConfig)) {
+      if (this.config[key] === null) {
+        this.config[key] = this.defaultConfig[key];
+      }
+    }
+
+    for (var key of Object.keys(this.config)) {
+      this.addConfigData(key);
+    }
+  }
+
+  writeData() {
+    localStorage.setItem(stateString, JSON.stringify(this.state));
+    localStorage.setItem(configString, JSON.stringify(this.config));
+  }
+
+  clearData() {
+    if (localStorage.getItem(stateString)) {
+      localStorage.removeItem(stateString);
+    }
+    if (localStorage.getItem(configString)) {
+      localStorage.removeItem(configString);
+    }
+  }
+}
+
+export { DataManager, numberConfig, enumConfig };
--- src/script.js
+++ src/script.js
@@ -1,23 +1,105 @@
 import "./style.css";
-import { TimeManager } from "./engine/time.js";
 import { WindowManager } from "./engine/window.js";
-import { CustomerRenderer } from "./engine/renderer.js";
+import { DataManager, enumConfig, numberConfig } from "./engine/data.js";
 import Stats from "stats-js";
-import { generateLoadingManager } from "./engine/loader.js";
-import { InputManager } from "./engine/input.js";
-import { GameEngine } from "./engine/engine.js";
-import { RadianceCascade } from "./examples/radiance_cascade/radiance_cascade.js";
 import { addCustomArrayMethods } from "./utils/array.js";
+import * as twgl from "twgl.js";
 
 addCustomArrayMethods();
 
+const windowManager = new WindowManager(1);
 var stats = new Stats();
 stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom);
+const canvas = document.getElementById("webgl");
+const gl = document.getElementById("webgl").getContext("webgl");
+
+const vs = `
+attribute vec4 position;
+
+void main() {
+  gl_Position = position;
+}
+`;
+const fs = `
+precision mediump float;
+
+uniform vec2 resolution;
+uniform float time;
+
+void main() {
+  vec2 uv = gl_FragCoord.xy / resolution;
+  float color = 0.0;
+  // lifted from glslsandbox.com
+  color += sin( uv.x * cos( time / 3.0 ) * 60.0 ) + cos( uv.y * cos( time / 2.80 ) * 10.0 );
+  color += sin( uv.y * sin( time / 2.0 ) * 40.0 ) + cos( uv.x * sin( time / 1.70 ) * 40.0 );
+  color += sin( uv.x * sin( time / 1.0 ) * 10.0 ) + sin( uv.y * sin( time / 3.50 ) * 80.0 );
+  color *= sin( time / 10.0 ) * 0.5;
+
+  gl_FragColor = vec4( uv, 0., 1.0 );
+}
+`;
+const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+
+const arrays = {
+  position: {
+    numComponents: 3,
+    data: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+  },
+};
+const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+const width = 256;
+const height = 256;
+
+const defaultData = {
+  state: {},
+  config: {
+    myEnum: enumConfig("My Enum", "v", ["1", "2", "3", "v"]),
+    myNum: numberConfig("My Num", 0, -1, 10, 0.1),
+  },
+};
+
+const data = new DataManager(defaultData);
+data.init();
+
+const frameBuffers = {
+  cascadeRT: twgl.createFramebufferInfo(gl, null, width, height),
+  cascadeRTSpare: twgl.createFramebufferInfo(gl, null, width, height),
+  finalCascadeRT: twgl.createFramebufferInfo(gl, null, width, height),
+  finalCascadeRTSpare: twgl.createFramebufferInfo(gl, null, width, height),
+};
+
+windowManager.listeners.push({
+  updateSize: ({ width, height }) => {
+    gl.canvas.width = width;
+    gl.canvas.height = height;
+  },
+});
+
+function render(time) {
+  windowManager.update();
+  twgl.resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  const uniforms = {
+    time: time * 0.001,
+    resolution: [gl.canvas.width, gl.canvas.height],
+  };
+
+  gl.useProgram(programInfo.program);
+  twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+  twgl.setUniforms(programInfo, uniforms);
+  twgl.bindFramebufferInfo(gl);
+  twgl.drawBufferInfo(gl, bufferInfo);
+
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
+/*
 
 const time = new TimeManager();
 const loader = generateLoadingManager();
-const windowManager = new WindowManager(1);
 const input = new InputManager(windowManager, time);
 const renderer = new CustomerRenderer(windowManager);
 
@@ -48,3 +130,4 @@ function raf() {
 }
 
 window.requestAnimationFrame(raf);
+*/
